ed0a886dab37f752f5aa293fbed00f2288d020b2
viniciusmilanez <vinicius.254711@alunosatc.edu.br>
2023-11-24 19:47:55 -0300

    feat: create ApiDroneApplication.java

---
+++ main/java/com/dronedelivery/apidrone/ApiDroneApplication.java
@@ -0,0 +1,13 @@
+package com.dronedelivery.apidrone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDroneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDroneApplication.class, args);
+	}
+
+}
